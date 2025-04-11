3b5b7a3dedcf3f0b4740add8224a158e5946e587
ZaEugene <y.h.zaika@student.khai.edu>
2025-04-11 12:55:57 +0300

    add feature/create-products-module

--- src/user/user.router.js
+++ src/user/user.router.js
@@ -11,17 +11,17 @@ router.post('/users', async (request, response) => {
     return response.sendStatus(201);
 });
 
+router.get('/users/:id/products', async (request, response) => {
+  const { id } = request.params;
+  const userProdusts = await db.query.products.findMany({
+      where: eq(products.userId, +id)
+  });
+  return response.json(userProdusts);
+});
+
 router.get('/users', async (request, response) => {
     const users = await db.query.users.findMany();
     return response.json(users);
-});
-
-router.get('/users/:id/products', async (request, response) => {
-    const { id } = request.params;
-    const userProdusts = await db.query.products.findMany({
-        where: eq(products.userId, +id)
-    });
-    return response.json(userProdusts);
 });
 
 module.exports = router;
